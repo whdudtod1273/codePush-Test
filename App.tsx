4b27a7fd5a014cfd47fa12a8b33eca8cf8c86c1c
whdudtod1273 <whdudtod1273@naver.com>
2021-11-29 17:31:49 +0900

    start

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>codePush Test</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
